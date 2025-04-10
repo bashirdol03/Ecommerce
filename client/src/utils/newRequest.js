34a6b8ec358a9d34fdb9e888d3c34b2f164f7b1d
Bashir Dol (dolb) <dolb@coventry.ac.uk>
2025-04-10 01:03:23 +0100

    added domain-7

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://api.mybackendserver.pro/api",
   withCredentials: true,
 });
 
